6fa366c1cee7a61ce32d08c11dcccf48aecfe57e
Joseph Kessler <kesslerjx@icloud.com>
2022-03-25 12:18:23 -0400

    Add onclick to all buttons

---
+++ scripts.js
@@ -0,0 +1,30 @@
+
+let screenValue;
+
+///////////////
+/* FUNCTIONS */
+///////////////
+
+function buttonPressed(element) {
+    console.log(element);
+}
+
+function operate(operator, first, second) {
+
+}
+
+function add(first, second) {
+    return first + second;
+}
+
+function subtract(first, second) {
+    return first - second;
+}
+
+function multiply(first, second) {
+    return first * second;
+}
+
+function divide(first, second) {
+   return first / second; 
+}
